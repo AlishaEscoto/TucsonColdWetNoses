be486a18535de3ba3a1d027cf11bd4b5215131e0
Alisha Escoto <hi@AlishaEscoto.com>
2016-12-14 10:17:27 -0700

    Gitignore and basic Underscores Starter
    I used the gitignore file from Salcode Github here:
    https://gist.github.com/salcode/b515f520d3f8207ecd04
    
    Also uploaded basic Underscores starter theme without any changes

---
+++ wp-content/themes/tucsoncoldwetnoses/footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package TucsonColdWetNoses
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'tucsoncoldwetnoses' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'tucsoncoldwetnoses' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'tucsoncoldwetnoses' ), 'tucsoncoldwetnoses', '<a href="https://www.alishaescoto.com" rel="designer">Alisha Escoto</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
