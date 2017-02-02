6cd86de19fe660ae0a758a2dd688960bf1f269ba
Alisha Escoto <hi@AlishaEscoto.com>
2017-02-02 14:02:13 -0700

    Added Analytics file and reference in header

--- wp-content/themes/tucsoncoldwetnoses/header.php
+++ wp-content/themes/tucsoncoldwetnoses/header.php
@@ -21,6 +21,7 @@
 </head>
 
 <body <?php body_class(); ?>>
+<?php include_once("analyticstracking.php") ?>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'tucsoncoldwetnoses' ); ?></a>
 
