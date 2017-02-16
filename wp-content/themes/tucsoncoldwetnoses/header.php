7acb7ce11941c87738d97c5e91a2dc8b25047972
Alisha Escoto <hi@AlishaEscoto.com>
2017-02-16 15:47:54 -0700

    Changed background image

--- wp-content/themes/tucsoncoldwetnoses/header.php
+++ wp-content/themes/tucsoncoldwetnoses/header.php
@@ -32,7 +32,7 @@
 <?php if ( get_header_image() ) { ?>
 	<header id="masthead" class="site-header" role="banner" style="background-image: url(<?php header_image(); ?>);">
 <?php } else { ?>
-	<header id="masthead" class="site-header" role="banner" style="background-image: url(/wp-content/themes/tucsoncoldwetnoses/images/Adoptable_Pets.jpg);">
+	<header id="masthead" class="site-header" role="banner" style="background-image: url(/wp-content/themes/tucsoncoldwetnoses/images/Pet-rescue.jpg);">
 <?php } //End Header Image Check ?>
 <div class="container">
 	<?php if ( has_custom_logo() ) {
